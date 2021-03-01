e51c726ff16caa3b83e3f102e4f38a02591ecc7d
Michal Czuper <mrczuper@gmail.com>
2021-03-01 18:17:07 +0200

    More note code

--- frontend/whiteboard/src/messages/note.js
+++ frontend/whiteboard/src/messages/note.js
@@ -6,17 +6,32 @@ const NOTE_WIDTH = 200
 export function promptForNote(context, coordinates) {
     let text = prompt("Please type in the text of your note:")
     if (text === null) return;
-    console.log(text)
     addNote(text, context, coordinates)
 }
 
 export function addNote(text, context, coordinates, addToMemoryToo = true) {
     context.font = "20px Arial";
-    context.fillStyle = "black";
+    context.fillStyle = "red";
     context.strokeRect(coordinates.x, coordinates.y, NOTE_WIDTH, NOTE_HEIGHT);
-    let height = context.measureText(text).actualBoundingBoxAscent
-    context.fillText(text, coordinates.x + 5, coordinates.y + height + 5, 200 - 5);
 
+    let textDimensions = context.measureText(text)
+    let height = textDimensions.actualBoundingBoxAscent
+    let width = textDimensions.width
+
+    // Currently broken
+    if (width > NOTE_WIDTH) { // Text too long, need multiple fillText calls
+        console.log("Splitting text")
+        let calls = Math.ceil(width / NOTE_WIDTH)
+        console.log(calls)
+        let textArray = text.match(/.{1,calls}/g || [])
+        console.log(textArray)
+        for (;calls > 0; calls--) {
+            context.fillText(textArray[calls], coordinates.x + 5, coordinates.y + height + 5)
+        }
+    } else {
+        console.log("Adding note text")
+        context.fillText(text, coordinates.x + 5, coordinates.y + height + 5);
+    }
 
     if (addToMemoryToo) {
         let obj = {
